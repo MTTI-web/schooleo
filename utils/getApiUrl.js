bb5807aadaf912734e50a16fa5d2af3771da922a
MTTI-web <69585514+MTTI-web@users.noreply.github.com>
2021-08-13 10:32:53 +0530

    dev api url changed

--- utils/getApiUrl.js
+++ utils/getApiUrl.js
@@ -1,6 +1,6 @@
 const getApiUrl = () => {
   return 'https://schooleo-api.herokuapp.com';
-  // return 'https://localhost:5000';
+  // return 'http://localhost:5000';
 };
 
 export default getApiUrl;
